57efdffa9bb2a5a8c77b8423da1da257febcd097
Dit Navn <dinmail@domain.dk>
2025-09-30 13:42:14 +0200

    Scafix v8.2: tilføjet Alfix overdækning 2025

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,1 @@
-const CACHE_NAME = 'scafix-v8.1';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll([
-    './','./index.html','./style.css','./print.css','./main.js','./dataset.js','./manifest.json'
-  ])));
-  self.skipWaiting();
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));
-});
+const C='scafix-v6';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html','./style.css','./print.css','./main.js','./complete_lists.json','./dataset.js']))) });self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>x!==C&&caches.delete(x))))).then(()=>self.clients.claim())});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
