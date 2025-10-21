b812d66ce89a2cd4b6a025eda6b629b8e92fbe5a
ReneLS365 <67069360+ReneLS365@users.noreply.github.com>
2025-10-21 08:04:15 +0200

    Add admin code to unknown item test

--- tests/tenant.spec.ts
+++ tests/tenant.spec.ts
@@ -38,6 +38,7 @@ describe('admin-update handler', () => {
       body: JSON.stringify({
         action: 'update_prices',
         firmId: 'test',
+        code: 'hemmelig',
         updates: { UNKNOWN: 10 }
       })
     });
