3d2176dc27ce79ac5f9928c7b3bda0c9742e0025
ReneLS365 <67069360+ReneLS365@users.noreply.github.com>
2025-11-02 20:59:38 +0100

    Restore CJS Lighthouse config for CLI

---
+++ .lighthouserc.cjs
@@ -0,0 +1,36 @@
+const chromeFlags = [
+  '--no-sandbox',
+  '--disable-setuid-sandbox',
+  '--headless=new',
+  '--disable-gpu',
+  '--disable-dev-shm-usage',
+];
+
+const collect = {
+  url: ['http://127.0.0.1:4173'],
+  numberOfRuns: 1,
+  chromeFlags,
+};
+
+if (process.env.CHROME_PATH) {
+  collect.chromePath = process.env.CHROME_PATH;
+}
+
+module.exports = {
+  ci: {
+    collect,
+    assert: {
+      assertions: {
+        'categories:performance': ['error', {minScore: 1}],
+        'categories:accessibility': ['error', {minScore: 1}],
+        'categories:best-practices': ['error', {minScore: 1}],
+        'categories:seo': ['error', {minScore: 1}],
+        'categories:pwa': ['error', {minScore: 1}],
+      },
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: '.lighthouse',
+    },
+  },
+};
