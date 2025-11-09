88ace4f8d8c389a12a066bbad8336f48a1800819
ReneLS365 <67069360+ReneLS365@users.noreply.github.com>
2025-11-09 11:56:11 +0100

    chore(security): remove hardcoded Auth0 secrets and fix Netlify scanning

---
+++ app/src/auth0-config.js
@@ -0,0 +1,5 @@
+// TODO: Rotate Auth0 Client Secret in Auth0 dashboard,
+// da den tidligere værdi har været committet i repoet.
+export const AUTH0_DOMAIN = 'dev-3xcigxvdwlymo1k6.eu.auth0.com'
+export const AUTH0_CLIENT_ID = 'EX0ZiQEHzJDkzbEGZS0J9VfszUN9ggLY'
+export const AUTH0_REDIRECT_URI = 'https://csmate.netlify.app'
